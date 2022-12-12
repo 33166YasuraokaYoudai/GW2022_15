72833e5a6cbfbbd8ce7175ad09d19fc49a95187b
33166安楽岡陽大 <33166@ojs.ac.jp>
2022-12-12 11:54:45 +0900

    12/12 卒業制作　ログイン画面の見た目のみできた

--- CdSearchSystem/Window1.xaml.cs
+++ CdSearchSystem/Window1.xaml.cs
@@ -10,21 +10,20 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace CdSearchSystem {
     /// <summary>
-    /// MainWindow.xaml の相互作用ロジック
+    /// Window1.xaml の相互作用ロジック
     /// </summary>
-    public partial class MainWindow : Window {
-        public MainWindow() {
+    public partial class Window1 : Window {
+        public Window1() {
             InitializeComponent();
         }
 
-        private void btTitle_Click(object sender, RoutedEventArgs e) {
-            Window1 window = new Window1();
-            window.Show();
+        private void Button_Click(object sender, RoutedEventArgs e) {
+
         }
+
     }
 }
